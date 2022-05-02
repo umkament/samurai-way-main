c67a9b279d50bc54672b871ae7afacf75e5764cb
umkament <97944243+umkament@users.noreply.github.com>
2022-05-02 16:29:21 +0300

    change files, delete new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import Technologies from "./Technologies";
+import Header from "./Header";
+
 
 function App() {
   return (
+     <BrowserRouter>
     <div className="App">
      Hello, samurai! Let's go!
+      <Header/>
+      <Technologies/>
     </div>
+     </BrowserRouter>
   );
 }
 
---
+++ src/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const  Technologies = () => {
+  return (
+     <div>
+       <ul>
+         <li>CSS</li>
+         <li>HTML</li>
+         <li>JS</li>
+         <li>REACT</li>
+       </ul>
+     </div>
+  )
+}
+
+export default Technologies;
